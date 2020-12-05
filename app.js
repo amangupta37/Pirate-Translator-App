391240fe43ba0f12f20a910683b9104756dd639a
amangupta37 <amangupta6121998@gmail.com>
2020-12-05 06:23:35 +0530

    added js file

---
+++ app.js
@@ -0,0 +1,37 @@
+//console.log("hello world !!")
+const btn = document.getElementById("active-btn");
+const input = document.getElementById("txt-style");
+const output =document.getElementById("output-box");
+
+const url = "https://api.funtranslations.com/translate/pirate.json";
+const encrypt_url= encodeURI(url);
+console.log(encrypt_url)
+
+function getdata(datain){
+    return encrypt_url + "?" + "text=" + datain;
+    
+}
+function handelerror(error)
+{
+    console.log("error occured", error);
+    alert("Server is down now try after sometime");
+}
+
+btn.addEventListener("click",function(){
+   
+    console.log(input.value);
+    const inputdata = input.value;
+
+    fetch(getdata(inputdata))
+    .then(response => response.json())
+    .then(json => {
+        const convertedtext = json.contents.translated;
+        output.innerText = convertedtext;
+    })
+
+    .catch(handelerror)
+  
+
+});
+
+
